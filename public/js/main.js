0c405a724b42a234fd9ba2ca40b012cf3f1200cd
phaybein <fabian.m.martinez@gmail.com>
2018-11-17 15:08:37 -0800

    added js for nav RE #nav #js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
 console.log('Main JS loaded');
+
+// GRAB NAV BUTTON
+const menuBtn = document.querySelector('.nav__menu__btn');
+
+// GRAB NAV MENU
+const mobileMenu = document.querySelector('.nav__menu--mobile');
+
+// TOGGLE CLASS ON NAV MENU
+const clickedBtn = () => {
+    mobileMenu.classList.toggle('active');
+};
+
+// ADD EVENT LISTENER FOR CLICK
+menuBtn.addEventListener('click', clickedBtn);
