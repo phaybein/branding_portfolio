18c2f500ada0daf2ff542e2df9dfe5854076203d
phaybein <fabian.m.martinez@gmail.com>
2018-11-25 08:48:13 -0800

    added smooth scroll for links and a toolbox.js RE #toolbox.js #smooth-scroll

--- public/js/toolbox.js
+++ public/js/toolbox.js
@@ -1,19 +1,3 @@
-console.log('Main JS loaded');
-
-// GRAB NAV BUTTON
-const menuBtn = document.querySelector('.nav__menu__btn');
-
-// GRAB NAV MENU
-const mobileMenu = document.querySelector('.nav__menu--mobile');
-
-// TOGGLE CLASS ON NAV MENU
-const clickedBtn = () => {
-    mobileMenu.classList.toggle('active');
-};
-
-// ADD EVENT LISTENER FOR CLICK
-menuBtn.addEventListener('click', clickedBtn);
-
 // SMOOTH SCROLLING
 $('#main-nav a').on('click', function (e) {
     if (this.hash !== "") {
